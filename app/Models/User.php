c3361462b831c14904a8936b0feab6354a342081
John George <Sci.JohnGeorge.2017@alexu.edu.eg>
2021-04-22 04:14:26 +0200

    Initiating Project, Installing sanctum, laravel-follow, laravel-ban, Creating Tweet model and tweets table.

---
+++ app/Models/User.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use Cog\Contracts\Ban\Bannable as BannableContract;
+use Cog\Laravel\Ban\Traits\Bannable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Overtrue\LaravelFollow\Followable;
+
+class User extends Authenticatable implements BannableContract
+{
+    use HasApiTokens, Followable, Bannable, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'date_of_birth',
+        'image',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function tweets(){
+        return $this->hasMany(Tweet::class);
+    }
+}
